39cd2745cc0b45ed6656fc637423ae8b42f05601
xenoninja <mgotzeus7@gmail.com>
2025-04-16 14:15:22 +0800

    solve year 2024 day 01 part 1

---
+++ python/solutions/2024/day_01/solution.py
@@ -0,0 +1,38 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/1
+
+from ...base import StrSplitSolution, answer
+
+
+class Solution(StrSplitSolution):
+    _year = 2024
+    _day = 1
+
+    @answer(2057374)
+    def part_1(self) -> int:
+        input_list = self.input
+        left_list = []
+        right_list = []
+        res = 0
+
+        for i in input_list:
+            [a, b] = i.split()
+            left_list.append(int(a))
+            right_list.append(int(b))
+
+        left_list.sort()
+        right_list.sort()
+
+        for i in range(len(input_list)):
+            res += abs(left_list[i] - right_list[i])
+
+        return res
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
